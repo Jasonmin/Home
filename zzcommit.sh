d41d91f25c5037b4db5b7c73843f874acaf96872
qiangzhou3 <qiangzhou3@iflytek.com>
2023-05-24 11:33:22 +0800

    f

---
+++ zzcommit.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+## 权限授予 chmod u+x *.sh
+# chmod u+x ./zzcommit.sh
+
+# git push
+
+# 获取提交信息
+message="`date +%y-%m-%d,%H:%M`"
+if [ "$1" != "" ] #前后保留空格
+then
+    message="$1"
+fi
+
+# 日志
+# echo $message
+
+# 提交并推送
+git add .
+git commit -m "$message"
+
+# 拉取
+git pull
+git push
